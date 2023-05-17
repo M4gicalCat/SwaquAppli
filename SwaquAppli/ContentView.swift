2f6e18766620dd383eef414adbacea7b3d8a4d5c
M4gicalCat <philippefaisandier@gmail.com>
2023-05-17 16:24:28 +0200

    started langing page

--- SwaquAppli/ContentView.swift
+++ SwaquAppli/ContentView.swift
@@ -10,12 +10,47 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Swaqua")
+                .font(.largeTitle)
+            Text("L'application qui vous aide à rester hydraté !")
+                .frame(maxWidth: 300, alignment: .center)
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                                // Action à exécuter lorsque le bouton est pressé
+                                // Vous pouvez ajouter votre propre code ici
+                                print("Le bouton a été pressé")
+                            }) {
+                                Text("Se connecter")
+                                    .font(.headline)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.init(red:23/255,green: 54/255, blue: 99/255))
+                                    .cornerRadius(10)
+                            }
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Button(action: {
+                                // Action à exécuter lorsque le bouton est pressé
+                                // Vous pouvez ajouter votre propre code ici
+                                print("Le bouton a été pressé")
+                            }) {
+                                Text("Créer un compte")
+                                    .font(.headline)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.init(red:23/255,green: 54/255, blue: 99/255))
+                                    .cornerRadius(10)
+                            }
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
